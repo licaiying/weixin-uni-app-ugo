85c77cc02db8908c74b671acaf1ca479531684f2
licaiying <1194449590@qq.com>
2020-04-05 20:20:50 +0800

    向购物车添加商品

--- src/pages/goods/index.vue
+++ src/pages/goods/index.vue
@@ -16,13 +16,15 @@
     </view>
     <!-- 商品详情 -->
     <view class="detail">
-      <rich-text></rich-text>
+      <!-- rich-text：小程序展示商品详情时，所特有的组件 -->
+      <rich-text :nodes="data.goods_introduce"></rich-text>
     </view>
     <!-- 操作 -->
     <view class="action">
+      <!-- open-type="contact":打开会话客服 -->
       <button open-type="contact" class="icon-handset">联系客服</button>
       <text class="cart icon-cart" @click="goCart">购物车</text>
-      <text class="add">加入购物车</text>
+      <text class="add" @tap="add">加入购物车</text>
       <text class="buy" @click="createOrder">立即购买</text>
     </view>
   </view>
@@ -32,7 +34,12 @@
   export default {
     data(){
       return {
-        data:null
+        data:null,
+        
+        // 1.往购物车添加商品时 ，首先判断，购物车里是否有该商品，若有，就让数量加1，若没有，就按新商品添加
+        // 根据本地存储的信息进行判断
+        // 可能没有数据，直接设为空对象
+        carts:uni.getStorageSync("carts") || []
       }
     },
 
@@ -43,13 +50,14 @@
           url: '/pages/cart/index'
         })
       },
+
       createOrder () {
         uni.navigateTo({
           url: '/pages/order/index'
         })
       },
 
-      // 根据商品的id，发请求，获取商品的详细信息，并在页面中做展示
+      // 根据商品的id，发请求，获取商品的详细信息，并在页面中做展示-----------------------------
       async getData(id){
         const res = await this.request({
           url:"/api/public/v1/goods/detail",
@@ -60,6 +68,48 @@
 
         // console.log(res)
         this.data = res
+      },
+
+      // 向购物车添加商品的执行函数-----------------------------------
+      add(){
+        // 2.准备 添加的商品 的详细数据信息对象
+        var one = {
+          goods_id:this.data.goods_id, 
+          goods_name:this.data.goods_name,
+          goods_small_logo:this.data.goods_small_logo,
+          goods_numb:1, // 商品的数量，默认为1
+          goods_price:this.data.goods_price
+        }
+
+        // 3. 添加到购物车，注意，判断商品是否重复
+        // 3.1 假设没有重复
+        var key = "未重复"
+
+        // 3.2 循环遍历，购物车里的商品，做重复验证
+        for (var i=0;i<this.carts.length;i++) {
+          // 如果重复了
+          if (this.carts[i].goods_id == one.goods_id) {
+            key = "已重复"
+            // 就让这个商品的数量加1
+            this.carts[i].goods_numb++
+            break
+          }
+        }
+
+        // 3.3 若 key = "未重复" ，则作为新的商品进行添加
+        if (key == "未重复") {
+          this.carts.push(one)
+        }
+
+        // 4.将页面中新添加的商品存储到本地
+        uni.setStorageSync("carts",this.carts)
+
+        // 5.提示信息
+        uni.showToast({
+          title:"商品添加成功",
+          icon:"none"
+        })
+
       }
     },
 
